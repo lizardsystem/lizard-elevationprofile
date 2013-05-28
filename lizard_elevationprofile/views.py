cdf6172e6705dbcbb84e5b3ecf7449b0f449fa74
Arjen Vrielink <arjen.vrielink@nelen-schuurmans.nl>
2013-05-28 10:35:15 +0200

    variable name srs changed to epsg

--- lizard_elevationprofile/views.py
+++ lizard_elevationprofile/views.py
@@ -4,11 +4,8 @@ from __future__ import unicode_literals
 from __future__ import print_function
 
 from django.utils.translation import ugettext as _
-# from django.core.urlresolvers import reverse
 from lizard_map.views import MapView
-# from lizard_ui.views import UiView
 from lizard_ui.layout import Action
-# check this
 from django.views.generic.base import View
 from django.http import HttpResponse
 #from django.utils import simplejson as json
@@ -19,7 +16,6 @@ class ElevationProfile(MapView):
     """Elevation Profile tmp stub"""
     template_name = 'lizard_elevationprofile/elevationprofile.html'
     page_title = _('Elevation Profile')
-    profiledata = _('profile data')
 
     @property
     def content_actions(self):
@@ -40,12 +36,11 @@ class ElevationData(View):
     """Get request bounds and linestring, respond json"""
     def get(self, request, *args, **kwargs):
         #parse srs of map, only get the number
-        mapsrs = request.GET.get('srs').split(':')[-1]
+        epsg_code = request.GET.get('srs').split(':')[-1]
         wkt_geom = request.GET.get('geom')
         # TODO: here the request to gislib / gislib service
         url = 'http://localhost:5000/rasterinfo/profile'
-        params = {'srs': mapsrs, 'geom': wkt_geom}
+        params = {'epsg': epsg_code, 'geom': wkt_geom}
         r = requests.get(url, params=params)
         elevation_profile = r.text
-        #print(elevation_profile)
         return HttpResponse(elevation_profile, content_type='application/json')
