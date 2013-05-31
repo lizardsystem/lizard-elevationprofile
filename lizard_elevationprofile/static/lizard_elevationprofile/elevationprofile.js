d9e06cec141b261f92ba076559e7ca21e2090431
Arjen Vrielink <arjen.vrielink@nelen-schuurmans.nl>
2013-05-31 11:16:10 +0200

    whitespace; code cleanup

--- lizard_elevationprofile/static/lizard_elevationprofile/elevationprofile.js
+++ lizard_elevationprofile/static/lizard_elevationprofile/elevationprofile.js
@@ -1,50 +1,58 @@
 // jslint configuration; btw: don't put a space before 'jslint' below.
-/*jslint browser: true, devel: true, sloppy: true */
+/*jslint browser: true, devel: true, sloppy: true, vars: true, es5: true */
 /*global $, OpenLayers, map*/
 
-// TODO: order of functions and vars
-
 (function () {
-    // function to draw elevation graph with flot jquery plugin
+    /** Draw elevation graph in popup with Flot jQuery plugin
+     */
     var drawElevationGraph = function (elevationData) {
+        var graphHTML = '<div id="elevation-profile" \
+                         style="width:400px;height:200px">Graph</div>';
+
+        // TODO: hardcoded 'profile': ugly
+        var elevationSeries = [{data: elevationData.profile}];
+        var plotOptions = {label: "Height",
+                           lines: {fill: true},
+                           grid: {clickable: true, hoverable: true}
+                          };
+
         // TODO: hmm, maybe just kill the first popup since we only have one?
         for (var existingPopup in map.popups) {
             map.removePopup(map.popups[existingPopup]);
         }
-        var options,
-            graphHTML = '<div id="elevation-profile" style="width:400px;height:200px">Graph</div>',
-            popup = new OpenLayers.Popup.FramedCloud(
+
+        var popup = new OpenLayers.Popup.FramedCloud(
                 "Elevation Profile",
-                map.getLonLatFromPixel((map.getControlsByClass("OpenLayers.Control.MousePosition")[0]).lastXy),
+                map.getLonLatFromPixel((map.getControlsByClass(
+                        "OpenLayers.Control.MousePosition")[0]).lastXy),
                 null,
                 graphHTML,
                 null,
                 true
             );
+
         map.addPopup(popup);
-        // TODO: hardcoded 'profile': ugly
-        var elevationSeries = [{data: elevationData['profile']}];
-        var plotOptions = {
-                            label: "Height", 
-                            lines: {fill: true},
-                            grid: {clickable: true, hoverable: true},
-                          };
         $.plot($("#elevation-profile"), elevationSeries, plotOptions);
     };
 
-    // function to setup DrawLineControl and add to OpenLayers map
+    /** Setup DrawLineControl and add to global OpenLayers map object
+     */
     var setupDrawLineControl = function () {
-        var lineLayer = new OpenLayers.Layer.Vector("Profile layer", {
-                                                    displayInLayerSwitcher: false}),
-            drawLineControl = new OpenLayers.Control.DrawFeature(lineLayer, OpenLayers.Handler.Path, {handlerOptions: {maxVertices: 2}});
+        var lineLayer = new OpenLayers.Layer.Vector("Profile layer",
+                {displayInLayerSwitcher: false});
+        var drawLineControl = new OpenLayers.Control.DrawFeature(lineLayer,
+                    OpenLayers.Handler.Path, 
+                    {handlerOptions: {maxVertices: 2}}
+                );
 
         map.addLayer(lineLayer);
 
+        // register featureadded event on lineLayer
         lineLayer.events.on({
             featureadded: function (event) {
-                var geometry = event.feature.geometry,
-                    mapSrs = map.getProjection(),
-                    url = 'elevationdata/'; // TODO: hardcoded shizzle
+                var geometry = event.feature.geometry; // profile line
+                var mapSrs = map.getProjection(); // map projection
+                var url = 'elevationdata/'; // TODO: hardcoded shizzle
                 wktGeometry = geometry.toString();
                 requestData = "&geom=" + wktGeometry +
                               "&srs=" + mapSrs;
@@ -54,11 +62,9 @@
         });
 
         return drawLineControl;
-          
     };
 
     // draw line for elevation profile and get data from server
-    // TODO: destroy features on finish
     var actionElevationProfile = function () {
         var drawLineControl = map.getControlsByClass('OpenLayers.Control.DrawFeature')[0];
 
@@ -82,7 +88,6 @@
         }
     };
 
-    //switch on elevation profile when button is clicked
     function setUpElevationProfile() {
         $('.map-elevationprofile').on("click", actionElevationProfile);
     }
